77d6cc68c2a33ceadd2650263bf23fa793d4faa8
Auspicious <info@auspicioussoft.com>
2024-10-25 13:13:52 +0530

    Add console log to loginAction

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -11,6 +11,7 @@ import { cookies } from "next/headers"
 export const loginAction = async (payload: any) => {
     try {
         const res: any = await loginService(payload)
+        console.log('res: ', res);
         if (res.data.success) {
             await signIn('credentials', {
                 email: payload.email,
