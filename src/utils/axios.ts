90ff68f9a09c787f655f229a6b23a90d5b13f114
Auspicious <info@auspicioussoft.com>
2024-10-28 13:08:01 +0530

    Fix SSL certificate issue and improve axios configuration

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -9,7 +9,7 @@ export const axiosInstance = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
-        'role' : 'admin'
+        'role' : 'admin' 
     },
     httpsAgent: new https.Agent({
         rejectUnauthorized: false,
