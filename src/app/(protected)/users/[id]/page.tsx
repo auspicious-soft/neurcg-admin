4357df735f0278511eb68caa7e07b71128c36389
Auspicious <info@auspicioussoft.com>
2024-11-15 18:34:49 +0530

    Refactor user profile page and avatar component to utilize getImageUrl utility

--- src/app/(protected)/users/[id]/page.tsx
+++ src/app/(protected)/users/[id]/page.tsx
@@ -16,6 +16,7 @@ import thumbimg4 from "@/assets/images/video4.png"
 import NeurcgCard from '@/components/NeurcgCard';
 import useSWR from 'swr';
 import { getASingleUserService } from '@/service/admin-service';
+import { getImageUrl } from '@/utils';
 
 // const CreditScores =[
 //   {
@@ -34,34 +35,35 @@ import { getASingleUserService } from '@/service/admin-service';
 //       value: 18,
 //     },
 // ] 
-const VideoData =[
-    {
-      id:1,
-      title: "Lorem Ipsum Dummy Title",
-      thumbnail: thumbimg1,
-      url: "https://youtu.be/K4TOrB7at0Y?si=zFMHw8k0jDjXiGMi"
-    },
-    {
-      id:2,
-      title: "Lorem Ipsum Dummy Title",
-      thumbnail: thumbimg2,
-    },
-    {
-      id:3,
-      title: "Lorem Ipsum Dummy Title",
-      thumbnail: thumbimg3
-    },
-    {
-      id:4,
-      title: "Lorem Ipsum Dummy Title",
-      thumbnail: thumbimg4
-    },
-  ]
+const VideoData = [
+  {
+    id: 1,
+    title: "Lorem Ipsum Dummy Title",
+    thumbnail: thumbimg1,
+    url: "https://youtu.be/K4TOrB7at0Y?si=zFMHw8k0jDjXiGMi"
+  },
+  {
+    id: 2,
+    title: "Lorem Ipsum Dummy Title",
+    thumbnail: thumbimg2,
+  },
+  {
+    id: 3,
+    title: "Lorem Ipsum Dummy Title",
+    thumbnail: thumbimg3
+  },
+  {
+    id: 4,
+    title: "Lorem Ipsum Dummy Title",
+    thumbnail: thumbimg4
+  },
+]
 const ProfilePage = () => {
   const [imagePreview, setImagePreview] = useState<string | null>(null);
-  const { id } = useParams(); 
+  const { id } = useParams();
   const { data, isLoading, error } = useSWR(`/admin/users/${id}`, getASingleUserService)
- 
+  console.log('data: ', data);
+
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       const reader = new FileReader();
@@ -76,99 +78,91 @@ const ProfilePage = () => {
       reader.readAsDataURL(e.target.files[0]);
     }
   };
-
+  const projectsData = data?.data?.data?.projects
+  console.log('projectsData: ', projectsData);
 
   return (
     <div className="">
       <h1 className="text-2xl font-bold mb-4 hidden">Customer Profile: {id}</h1>
-     
+
       {/* Add more profile information here */}
-      <div className="bg-white rounded-[8px] p-5 md:px-[52px] md:py-[45px]"> 
-          <div className="flex md:flex-row flex-col gap-y-4 justify-between md:items-center mb:mb-10">
+      <div className="bg-white rounded-[8px] p-5 md:px-[52px] md:py-[45px]">
+        <div className="flex md:flex-row flex-col gap-y-4 justify-between md:items-center mb:mb-10">
           <div className="custom relative w-[177px] min-w-[177px] h-[177px] ">
-          <input
-            className="absolute top-0 left-0 h-full w-full opacity-0 p-0 cursor-pointer"
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-          />
-          {imagePreview ? (
-            <div className="relative h-full">
-              <Image
-                src={imagePreview}
-                alt="Preview"
-                width={177}
-                height={177}
-                fill
-                className="rounded-full h-full object-cover"
-              />
-            </div>
-          ) : (
-            <div className="grid place-items-center h-full w-full">
-              <div>
+            <input
+              className="absolute top-0 left-0 h-full w-full opacity-0 p-0 cursor-pointer"
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+            {imagePreview ? (
+              <div className="relative h-full">
                 <Image
-                  src={previmg2}
-                  alt="upload"
+                  src={imagePreview}
+                  alt="Preview"
                   width={177}
                   height={177}
-                  className="rounded-full"
+                  fill
+                  className="rounded-full h-full object-cover"
                 />
               </div>
-            </div>
-          )}
-        </div>
-        {/* <div className="grid gap-[6px] w-full">
-          {CreditScores.map((item)=>(
-            <CreditScore 
-            key={item.id}
-            text={item.text}
-            value={item.value}
-            />
-          ))}
-        </div> */}
+            ) : (
+              <div className="grid place-items-center h-full w-full">
+                <div>
+                  <Image
+                    src={previmg2}
+                    alt="upload"
+                    width={177}
+                    height={177}
+                    className="rounded-full"
+                  />
+                </div>
+              </div>
+            )}
           </div>
-          <div className=' mt-[30px] '>
-            <h1 className='text-xl md:text-[28px] text-[#3A2C23] font-semibold mb-[15px]'>Marisa Love</h1>
-            <div className='main-wrap flex justify-between items-center mb-3'>
+        </div>
+        <div className=' mt-[30px] '>
+          <h1 className='text-xl md:text-[28px] text-[#3A2C23] font-semibold mb-[15px]'>Marisa Love</h1>
+          <div className='main-wrap flex justify-between items-center mb-3'>
             <p className='title'>Email Address</p>
             <p className='values'>{data?.data?.data?.user?.email}</p>
-            </div>
-            <div className='main-wrap flex justify-between items-center mb-3'>
+          </div>
+          <div className='main-wrap flex justify-between items-center mb-3'>
             <p className='title'>Date Of Birth</p>
             <p className='values'>{data?.data?.data?.user?.dob}</p>
-            </div>
-            <div className='main-wrap flex justify-between items-center mb-3'>
+          </div>
+          <div className='main-wrap flex justify-between items-center mb-3'>
             <p className='title'>Phone Number</p>
             <p className='values'>+1 545 458 5236</p>
-            </div>
-            <div className='main-wrap flex justify-between items-center mb-3'>
+          </div>
+          <div className='main-wrap flex justify-between items-center mb-3'>
             <p className='title'>Home Address</p>
             <p className='values'>1024, lorents road</p>
-            </div>
-            <div className='main-wrap flex justify-between items-center '>
+          </div>
+          <div className='main-wrap flex justify-between items-center '>
             <p className='title'>City And State</p>
             <p className='values'>Arizona, US</p>
-            </div>
           </div>
-    </div>
-    <div className='grid gap-5 md:grid-cols-2 my-5 md:my-[50px] '>
-    <AvatarsCreated data= {data?.data?.data?.avatarsUsed}/>
-    <Referral data = {data?.data?.data?.user}/> 
-    </div>
-    <section className=''>
+        </div>
+      </div>
+      <div className='grid gap-5 md:grid-cols-2 my-5 md:my-[50px] '>
+        <AvatarsCreated data={data?.data?.data?.avatarsUsed} />
+        <Referral data={data?.data?.data?.user} />
+      </div>
+      <section className=''>
         <h2 className='section-title mb-[10px] md:mb-5'>Projects Created</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-5">
-        {/* data?.data?.data?.projects */}
-        {VideoData.map((data) =>(
+
+          {projectsData?.map((data: any, _: any) => (
             <NeurcgCard
-            key= {data.id}
-            title={data.title}
-            thumbnail={data.thumbnail}
-            url={data.url}
+              key={data?._id}
+              title={data?.projectName}
+              thumbnail={getImageUrl(data?.projectAvatar)}
+              url={data?.projectVideoLink}
             />
-        ))}
+          ))}
         </div>
-         </section>
+      </section>
 
     </div>
   );
--- src/components/AvatarsCreated.tsx
+++ src/components/AvatarsCreated.tsx
@@ -6,20 +6,8 @@ import img1 from "@/assets/images/video1.png";
 import img2 from "@/assets/images/video2.png";
 import img3 from "@/assets/images/video3.png";
 import img4 from "@/assets/images/video4.png";
+import { getImageUrl } from '@/utils';
 
-// interface AvatarProps {
-//   images: string[];  // Array of image URLs
-// }
-const avatarUrls = [
-  img1,
-  img2,
-  img3,
-  img4,
-  img1,
-  img2,
-  img3,
-  img4,
-];
 //const UserTable: React.FC<UserTableProps> = ({ customers }) => {
 const AvatarsCreated = (props: any) => {
   const { data } = props
@@ -27,10 +15,12 @@ const AvatarsCreated = (props: any) => {
     <div className="bg-white rounded-[8px] p-5 md:p-[30px] ">
       <h2 className="section-title mb-6">Avatars Created</h2>
       <div className="grid grid-cols-2 md:grid-cols-5 gap-[14px] justify-center">
-        {avatarUrls.map((src, index) => (
+        {data?.slice(0, 10)?.map((src:any, index:any) => (
           <div key={index} >
             <Image
-              src={src}
+              width={100}
+              height={100}
+              src={getImageUrl(src)}
               alt={`Avatar ${index + 1}`}
               objectFit="cover"  // Ensures the image covers the space className="w-[86px] h-[86px] relative"
               className="rounded-[4px] w-full aspect-square object-cover"
