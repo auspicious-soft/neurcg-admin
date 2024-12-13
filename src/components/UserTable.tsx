224331ce7788b0d527536fefd04a122935712d6a
Auspicious <info@auspicioussoft.com>
2024-12-13 10:55:10 +0530

    Add eye icon to view profile button in UserTable component

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -3,9 +3,9 @@ import { useRouter } from 'next/navigation';
 import ReactPaginate from 'react-paginate';
 import { NextLabel, PrevLabel } from '@/utils/svgIcons';
 import ReactLoading from 'react-loading';
-
+import { MdRemoveRedEye } from 'react-icons/md';
 const UserTable = (props: any) => {
-  const { usersData , setQuery, isLoading} = props;
+  const { usersData, setQuery, isLoading } = props;
   const total = usersData?.total ?? 0;
   const rowsPerPage = 10;
   const router = useRouter();
@@ -35,31 +35,31 @@ const UserTable = (props: any) => {
           </thead>
           <tbody>
             {
-            users.length > 0 ? (
-              users
-                .map((customer: any, index: number) => (
-                  <tr key={customer._id} className={`${index % 2 === 0 ? 'bg--100' : 'bg-whi'}`}>
-                    <td>#{customer._id.slice(0, 5)}</td>
-                    <td>{customer.firstName + ' ' + customer.lastName}</td>
-                    <td>{customer.planType[0].toUpperCase() + customer.planType.slice(1)}</td>
-                    <td>{customer.email}</td>
-                    <td>
-                      <button
-                        onClick={() => handleViewProfile(customer._id)}
-                        className="text-[#26395E] hover:underline"
-                      >
-                        View
-                      </button>
-                    </td>
-                    <td>{customer.creditsLeft}</td>
-                  </tr>
-                ))
-            ) : (
-              <tr className="w-full flex justify-center p-3 items-center">
-                 {isLoading ?  <ReactLoading type={'spin'} color={'#e87223'} height={'40px'} width={'40px'} /> : 'No data found'}
-              </tr>
-            )
-            
+              users.length > 0 ? (
+                users
+                  .map((customer: any, index: number) => (
+                    <tr key={customer._id} className={`${index % 2 === 0 ? 'bg--100' : 'bg-whi'}`}>
+                      <td>#{customer._id.slice(0, 5)}</td>
+                      <td>{customer.firstName + ' ' + customer.lastName}</td>
+                      <td>{customer.planType[0].toUpperCase() + customer.planType.slice(1)}</td>
+                      <td>{customer.email}</td>
+                      <td>
+                        <button
+                          onClick={() => handleViewProfile(customer._id)}
+                          className="text-[#26395E] hover:underline"
+                        >
+                          <MdRemoveRedEye className='text-2xl text-[#E87223] hover:underline' />
+                        </button>
+                      </td>
+                      <td>{customer.creditsLeft}</td>
+                    </tr>
+                  ))
+              ) : (
+                <tr className="w-full flex justify-center p-3 items-center">
+                  {isLoading ? <ReactLoading type={'spin'} color={'#e87223'} height={'40px'} width={'40px'} /> : 'No data found'}
+                </tr>
+              )
+
             }
           </tbody>
         </table>
